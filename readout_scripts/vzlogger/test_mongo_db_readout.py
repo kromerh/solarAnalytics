2e87a5877f9268b39be1f47974f7c867c5f38940
Heiko Kromer <heiko.kromer@gmail.com>
2019-11-09 13:24:55 +0100

    newest

--- readout_scripts/vzlogger/test_mongo_db_readout.py
+++ readout_scripts/vzlogger/test_mongo_db_readout.py
@@ -6,14 +6,17 @@ import re
 # Choose the appropriate client
 client = MongoClient()
 
-# Connect to the db solarAnlage
-db = client.solarAnlage
+# Connect to the db paulisasnake
+db = client.paulisasnake
 
-# Use the collection vzlogger
-vzlogger = db.vzlogger
+# Use the collection temp and humid
+coll = db.temp_and_humid
+
+# Bulk inserting documents. Each row in the DataFrame will be a document in Mongo
+coll.insert_many(data.to_dict('records'))
 
 
 # ====== Finding Documents ====== #
-documents = vzlogger.find()
-df = pd.DataFrame(list(documents))
-print(df)
+documents = coll.find()
+data = pd.DataFrame(list(documents))
+print(data)
